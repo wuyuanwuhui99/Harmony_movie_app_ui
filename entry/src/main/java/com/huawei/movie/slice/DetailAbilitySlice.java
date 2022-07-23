d3196f058141e30bcc1d92efbd7ae68bf5aeec4a
wuyuanwuhui99 <275018723@qq.com>
2022-07-23 23:41:22 +0800

    电影详情页布局

--- entry/src/main/java/com/huawei/movie/slice/DetailAbilitySlice.java
+++ entry/src/main/java/com/huawei/movie/slice/DetailAbilitySlice.java
@@ -2,9 +2,14 @@ package com.huawei.movie.slice;
 
 import com.alibaba.fastjson.JSON;
 import com.huawei.movie.ResourceTable;
+import com.huawei.movie.config.Api;
+import com.huawei.movie.config.Config;
 import com.huawei.movie.entity.MovieEntity;
+import com.huawei.movie.utils.HttpRequest;
 import ohos.aafwk.ability.AbilitySlice;
 import ohos.aafwk.content.Intent;
+import ohos.agp.components.Image;
+import ohos.agp.components.Text;
 
 import java.io.Serializable;
 
@@ -16,6 +21,7 @@ public class DetailAbilitySlice extends AbilitySlice {
         super.setUIContent(ResourceTable.Layout_ability_detail);
         String movieItem = getAbility().getIntent().getStringParam("movieItem");
         movieEntity = JSON.parseObject(movieItem, MovieEntity.class);
+        initUI();
     }
 
     @Override
@@ -26,5 +32,23 @@ public class DetailAbilitySlice extends AbilitySlice {
     @Override
     public void onForeground(Intent intent) {
         super.onForeground(intent);
+    }
+
+    /**
+     * @desc 初始化UI
+     * @since 2022-07-23
+     * */
+    private void initUI(){
+        Text movieName = (Text)findComponentById(ResourceTable.Id_detail_movie_name);
+        movieName.setText(movieEntity.getMovieName());
+        if(movieEntity.getDescription() != null){
+            Text description = (Text)findComponentById(ResourceTable.Id_detail_description);
+            description.setText(movieEntity.getDescription());
+        }
+        Text star = (Text)findComponentById(ResourceTable.Id_detail_star);
+        star.setText(movieEntity.getStar());
+        Image image = (Image)findComponentById(ResourceTable.Id_detail_movie_img);
+        image.setCornerRadius(Config.imgRadius);
+        HttpRequest.setImages(this,image, Api.PROXY + movieEntity.getLocalImg());
     }
 }
